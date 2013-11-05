e37ef52da8dcf8b4f9d3500ab65c5c984a533c89
X3N0-Life-Form <adrarsis@laposte.net>
2013-11-05 17:37:06 +0100

    parsing is starting to work

--- Graph.cpp
+++ Graph.cpp
@@ -9,25 +9,27 @@
 
 #include <fstream>
 #include <iostream>
+#include <algorithm>
 
 Graph::Graph(const std::string fileUrl) {
 	std::ifstream ifs(fileUrl);
-	std::cout << "open!" << fileUrl << std::endl;
+	std::cout << "Opening file: " << fileUrl << std::endl;
 	while (ifs.is_open() && !ifs.eof()) {
 		std::string line;
 		getline(ifs, line);
 		std::cout << "current line:" << line << std::endl;
 		if (line.find("e") == 0) { //line starts with 'e'
 			parseLine(line);
-		} else if (line.find("c") == 0 &&
-				line.find("number of vertices") != std::string::npos) {
-			std::string::size_type pos = line.find_last_of(":");
+		} else if (line.find("c") == 0
+				&& nodes.size() == 0
+				&& line.find("number of vertices") != std::string::npos) {
+			std::string::size_type pos = line.find_last_of(":") + 1;
 			line = line.substr(pos);
-			int numberOfNodes(125);
-			//int numberOfNodes = std::stoi(line); //TODO:number of nodes
-			for (int i = 0; i < numberOfNodes; i++) {
+			int numberOfNodes = std::stoi(line); //TODO:number of nodes
+			for (int i = 1; i < numberOfNodes; i++) {
 				this->nodes.insert(NODE_INSERT(i));
 			}
+			std::cout << "Created " << numberOfNodes << " nodes" << std::endl;
 		}
 		//std::cout << "end loop" << std::endl;
 	}
@@ -41,6 +43,10 @@ Graph::~Graph() {
 void Graph::parseLine(std::string line) {
 	std::string temp = line.substr(line.find_first_of(" "), line.find_last_of(" "));
 	int origin = std::stoi(temp);
-	//int target = stoi(temp);
+
+	temp = line.substr(line.find_last_of(" "));
+	int target = stoi(temp);
+
+	nodes.at(origin).addNeighbor(nodes.at(target));
 }
 
