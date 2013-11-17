329b5dfb1551055747db6438c665691ff6d4a676
X3N0-Life-Form <adrarsis@laposte.net>
2013-11-17 18:20:16 +0100

    now printing results in a test file

--- main.cpp
+++ main.cpp
@@ -1,22 +1,20 @@
 #include "Node.h"
 #include "Graph.h"
 #include "Greedy.h"
+#include <fstream>
 
 #define DEFAULT_FILE_NAME "res/c FILE:  C125.9.clq"
 
 int main() {
-	Graph g("res/c250.9.clq");
+	std::string fileName = "res/c250.9.clq";
+	Graph g(fileName);
 	std::cout << "Creating Greedy object ..." << std::endl;
 	Greedy greedo(g);
 	std::cout << "Running findMax method ..." << std::endl;
 	Graph result = greedo.findMax();
-	std::cout << result;
-	/*TODO:
-	 * recup clique max
-	 * operator<< dans graph
-	 * écrire résultats dans un fichier
-	 * format:
-	 * 	Graphe: nombre de noeuds:
-	 * 		Noeud n°X: idVoisin1, idVoisin2, ...
-	 */
+
+	std::ofstream ofs(fileName.append(".result"));
+	ofs << result;
+	std::cout << "Results printed in " << fileName;
+	ofs.close();
 }
