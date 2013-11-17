b95ff7a8042f377efaf3e6b2286b816249416176
wyrllyn <etudiant@LKQ.localdomain>
2013-11-17 17:25:19 +0100

    another file

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #define DEFAULT_FILE_NAME "res/c FILE:  C125.9.clq"
 
 int main() {
-	Graph g(DEFAULT_FILE_NAME);
+	Graph g("res/c250.9.clq");
 	std::cout << "Creating Greedy object ..." << std::endl;
 	Greedy greedo(g);
 	std::cout << "Running findMax method ..." << std::endl;
